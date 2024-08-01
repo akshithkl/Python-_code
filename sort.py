d8770466788c578296c9a9fd6db425b6e3080c03
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2024-08-01 12:06:40 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,7 @@
+
+a = [12,34,3,7,5,9,7,5,3]   # sort list and it is implace operation, it is sort whole list
+#a.sort()
+#print(a)
+
+A=sorted(a)                 # sorted list and it is nondistructive sorting, It is not sort whole list
+print(A)
