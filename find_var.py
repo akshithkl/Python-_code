384a4b97cf712dfbfe8fc95a22eb010219aa798e
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2024-08-13 09:45:52 +0530

    Add files via upload

---
+++ find_var.py
@@ -0,0 +1,8 @@
+name= "the name is akshith"
+user_input=input('Enter whatever:')
+
+if user_input in name:
+    print(f"name variable has--->{user_input}")
+
+else:
+    print("does'nt have any variable")
